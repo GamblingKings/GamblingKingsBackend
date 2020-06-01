e06221cadc76c6b02e2fdf002675fd4d70d3fefd
Vincent Wong <vwong125@my.bcit.ca>
2020-06-01 16:56:17 -0700

    initial websocket setup

---
+++ src/handler.ts
@@ -0,0 +1,49 @@
+import db from "./module/db";
+
+interface putParamsObj {
+  TableName: string;
+  Item: {
+    connectionId: string;
+  };
+}
+
+interface deleteParamsObj {
+  TableName: string;
+  Key: {
+    connectionId: string;
+  };
+}
+
+async function connectionManager(event: any): Promise<object> {
+  if (event.requestContext.eventType === "CONNECT") {
+    const putParams: putParamsObj = {
+      TableName: process.env.ConnectionDynamoDBTable,
+      Item: {
+        connectionId: event.connectionId,
+      },
+    };
+
+    return await db.put(putParams).promise();
+  } else if (event.requestContext.eventType === "DISCONNECT") {
+    const deleteParams: deleteParamsObj = {
+      TableName: process.env.ConnectionDynamoDBTable,
+      Key: {
+        connectionId: event.connectionId,
+      },
+    };
+
+    return await db.delete(deleteParams).promise();
+  }
+}
+
+async function defaultMessage(event, context) {
+  return {
+    status: 403,
+    event: event,
+  };
+}
+
+module.exports = {
+  connectionManager,
+  defaultMessage,
+};
