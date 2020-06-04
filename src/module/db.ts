190d400212376e0596c2602489b03abd7c8e27c2
Patrick Jiang <jzc1828@gmail.com>
2020-06-03 17:32:08 -0700

    Improved db.ts to take env variables to determine local or prod

--- src/module/db.ts
+++ src/module/db.ts
@@ -1,10 +1,31 @@
+import * as dotenv from 'dotenv';
 import { DynamoDB } from 'aws-sdk';
 
-const DB = new DynamoDB.DocumentClient({
-  apiVersion: '2012-08-10',
-  // Uncomment for local dev only
-  // region: 'localhost',
-  // endpoint: 'http://localhost:8000',
-});
+// Load env variables from .env
+dotenv.config();
+
+interface DynamoDBOption {
+  apiVersion: string;
+  region?: string;
+  endpoint?: string;
+}
+
+const getDefaultOptions = (): DynamoDBOption => {
+  const fixedOption = { apiVersion: '2012-08-10' };
+
+  // For local
+  if (process.env.ENVIRONMENT && process.env.ENVIRONMENT === 'local') {
+    return {
+      ...fixedOption,
+      region: 'localhost',
+      endpoint: 'http://localhost:8000',
+    };
+  }
+
+  // For prod
+  return fixedOption;
+};
+
+const DB = new DynamoDB.DocumentClient(getDefaultOptions());
 
 export default DB;
