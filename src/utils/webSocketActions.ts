88d0ef4c4d86ce0a4a1b883d851762bb29ec72af
Patrick Jiang <jzc1828@gmail.com>
2020-06-06 14:29:48 -0700

    Addee comments

--- src/utils/webSocketActions.ts
+++ src/utils/webSocketActions.ts
@@ -2,6 +2,11 @@ import { LambdaEventBodyPayloadOptions, WebSocketResponse, WebSocketActions } fr
 import { User } from '../models/User';
 import { Game } from '../models/Game';
 
+/**
+ * Create a websocket response object
+ * @param {WebSocketActions} action one of the actions from WebSocketActions
+ * @param {LambdaEventBodyPayloadOptions} payload one of the payload options from LambdaEventBodyPayloadOptions
+ */
 export const createWSResponse = (
   action: WebSocketActions,
   payload: LambdaEventBodyPayloadOptions,
@@ -12,22 +17,31 @@ export const createWSResponse = (
   };
 };
 
+/**
+ * Create SEND_MESSAGE response object
+ * @param {string} message message
+ */
 export const createWSMessageResponse = (message: string): string => {
-  // Create message response object
   const wsPayload = { message } as LambdaEventBodyPayloadOptions;
   const wsResponse = createWSResponse(WebSocketActions.SEND_MESSAGE, wsPayload);
   return JSON.stringify(wsResponse);
 };
 
+/**
+ * Create GET_ALL_USERS response object
+ * @param {User[]} users an array of User objects
+ */
 export const createWSAllUsersResponse = (users: User[]): string => {
-  // Create get all users response object
   const wsPayload = { users } as LambdaEventBodyPayloadOptions;
   const wsResponse = createWSResponse(WebSocketActions.GET_ALL_USERS, wsPayload);
   return JSON.stringify(wsResponse);
 };
 
+/**
+ * Create GET_ALL_GAMES response object
+ * @param {Game[]} users an array of Game objects
+ */
 export const createWSAllGamesResponse = (games: Game[]): string => {
-  // Create get all games response object
   console.log(games);
   const wsPayload = { games } as LambdaEventBodyPayloadOptions;
   const wsResponse = createWSResponse(WebSocketActions.GET_ALL_GAMES, wsPayload);
