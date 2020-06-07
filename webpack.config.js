c072a83b3784c4da0270bbcd0b9323403499c796
Patrick Jiang <jzc1828@gmail.com>
2020-06-07 14:59:56 -0700

    Use webpack to bundle lambda functions over using lambda layer

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
+
+const { isLocal } = slsw.lib.webpack;
+
+module.exports = {
+  mode: isLocal ? 'development' : 'production',
+  devtool: isLocal ? 'source-map' : 'none',
+  entry: slsw.lib.entries,
+  target: 'node',
+  resolve: {
+    extensions: ['.mjs', '.ts', '.js'],
+  },
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+};
