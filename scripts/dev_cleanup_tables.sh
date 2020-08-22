a80b603d9e4925bdb24dbee3c612609291f0f46f
Patrick Jiang <21091475+jzc1828@users.noreply.github.com>
2020-08-21 19:39:38 -0700

    Updated scripts

--- scripts/dev_cleanup_tables.sh
+++ scripts/dev_cleanup_tables.sh
@@ -4,6 +4,7 @@ SEPARATOR="--------------------------------------------------"
 ENDPOINT="http://localhost:8000"
 CONNECTIONS="Connections"
 GAMES="Games"
+GAMES_STATE="GameState"
 
 echo $SEPARATOR
 echo "Deleting $CONNECTIONS table at $ENDPOINT"
@@ -16,5 +17,12 @@ echo $SEPARATOR
 echo "Deleting $GAMES table at $ENDPOINT"
 aws dynamodb delete-table \
     --table-name $GAMES \
+    --endpoint-url $ENDPOINT
+echo $SEPARATOR
+
+echo $SEPARATOR
+echo "Deleting $GAMES_STATE table at $ENDPOINT"
+aws dynamodb delete-table \
+    --table-name $GAMES_STATE \
     --endpoint-url $ENDPOINT
 echo $SEPARATOR
